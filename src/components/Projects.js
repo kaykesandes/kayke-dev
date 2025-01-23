ca76403a585c3f984d66b8e6147284f9c66ddd2f
kaykesandes <kaykegy@proton.me>
2025-01-23 20:44:53 -0300

    nova att VAMO EMBORA

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,15 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Tab, Nav, Alert } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projDip1 from "../assets/img/project-img-diplomas1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
-export const Projects = () => {
+export const Projects = ({ status, message, onValidated }) => {
 
   const projects = [
     {
@@ -28,8 +30,11 @@ export const Projects = () => {
       imgUrl: projImg3,
       url: "https://github.com/kaykesandes/get_next_line",
     },
-    
+
   ];
+
+
+
 
   return (
     <section className="project" id="projects">
@@ -38,45 +43,61 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projetos</h2>
-                <p>Lista de projetos prontos, desde mais complexos até o mais simples.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Projetos</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Enviar mensagem</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Observações</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projetos</h2>
+                  <p>Lista de projetos prontos, desde os mais complexos até os mais simples.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Formação acadêmica</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Projetos</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Experiências profissionais</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <div className="project-container">
+                          <h3 className="title-project-diploma">Diploma de ensino superior</h3>
+                          <img src={projDip1} className="img-project" alt="Diploma de ensino superior"></img>
+                          <h4 className="project-title-curso">Ciência da Computação</h4>
+                          <p className="p-project-text">
+                            5 semestres de estudo, com foco em programação, matemática e lógica de programação.
+                          </p>
+                          <p className="p-project-date">Previsão de conclusão: 12/2026</p>
+                        </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
                                 />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Meu hobby preferido são estudar C++, ler livros, conversar com pessoas sobre assuntos interessantes, atualmente jogando Monster Hunter Rise</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                        Trabalhei 2 anos em escritórios com administração e agora estou em busca de me especializar na área de Computação <br/>
+                        <hr/>
+                          <strong>Recanto dos Doces Auxiliar administrativo</strong>, minha tarefa diária era entrada de estoque e lançamento de contas a pagar, planilhas excel para controle e relatorio de financeiro.<br/>
+                          <br/><strong>2024-2025</strong><br/>
+                          <hr/>
+                          <strong>Contabilidade Celina Estagio em administração</strong>, minha rotinia diária foi aprender e abosrver o maximo de conhecimento sobre a area de contabilidade, lançamento de notas fiscais, controle de estoque e atendimento ao cliente.
+                          <br/><strong>2023-2024</strong><br/>
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
